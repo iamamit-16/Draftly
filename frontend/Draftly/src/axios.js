9a1d7ea98b93130d858158d12967acf32a6ed6cb
iamamit-16 <iamakumar1612@gmail.com>
2026-02-21 13:40:24 +0530

    fix: use relative baseURL in axios to resolve production network errors

--- frontend/Draftly/src/axios.js
+++ frontend/Draftly/src/axios.js
@@ -1,10 +1,19 @@
+// import axios from "axios";
+
+// // in production, there's no localhost so we have to make this dynamic
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+// const api = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// export default api;
+
 import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
+// This is the safest way. It will always use whatever domain you are on.
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api", 
 });
 
 export default api;
