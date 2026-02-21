5145567c414297f262e03100c49788a75575c7fb
iamamit-16 <iamakumar1612@gmail.com>
2026-02-21 11:59:03 +0530

    change again in path

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv"
+
 import cors from "cors"
 import path from "path"
 
 
-dotenv.config();
 
 const app = express();
 const PORT =process.env.PORT||5001
