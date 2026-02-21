75696b70f472b0a0049a9ec018f7a45c17d86575
iamamit-16 <iamakumar1612@gmail.com>
2026-02-21 10:58:14 +0530

    add production url

--- frontend/Draftly/src/axios.js
+++ frontend/Draftly/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 //No localhost in production so make this dynamic
-const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api";
+const BASE_URL = import.meta.env.MODE === "development" 
+    ? "http://localhost:5001/api" 
+    : "https://draftly-1dpq.onrender.com/api";
 const api = axios.create({
     baseURL:BASE_URL
 })
