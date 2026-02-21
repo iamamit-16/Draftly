db3afa61869160c81bdc7367e7a5752a89349938
iamamit-16 <iamakumar1612@gmail.com>
2026-02-21 12:09:43 +0530

    change again in path

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,30 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
+import cors from "cors"
+import path from "path"
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 
-import cors from "cors"
-import path from "path"
+dotenv.config();
+
 
 
 
 const app = express();
-const PORT =process.env.PORT||5001
+
+const PORT = process.env.PORT || 5001
 const __dirname = path.resolve() // backend source
 
-
-app.use(express.json());
-
-
 //middleware
-app.use(express.json());//this middleware will parse json body:req.body
 
 if(process.env.NODE_ENV!="production"){
     app.use(cors({
     origin:"http://localhost:5173"
 }));
 }
+
+app.use(express.json());//this middleware will parse json body:req.body
 
 
 app.use("/api/notes",notesRoutes);
@@ -35,7 +34,7 @@ app.use("/api/notes",notesRoutes);
 if (process.env.NODE_ENV=="production"){
     app.use(express.static(path.join(__dirname,"../frontend/Draftly/dist")));
 
-    app.get("/",(req,res)=>{
+    app.get("/*",(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend/Draftly","dist","index.html"))
 })
 }
